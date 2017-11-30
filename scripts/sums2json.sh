ae90bb2a153551d7257022da0fb42825c070543c
Yaobo Xu <yx2@sanger.ac.uk>
2017-11-30 16:15:08 +0000

    add curl time out

--- scripts/sums2json.sh
+++ scripts/sums2json.sh
@@ -10,15 +10,15 @@ cat $IN_FILE | tee >(md5sum > md5.checksum) | sha512sum > sha2.checksum
 exit_code=$?
 
 # exit with error code of any command that fails from here
-set -e 
+set -e
 
 if [ $exit_code -eq 0 ]
 then
-  md5=`cut -d ' ' -f1 md5.checksum`
-  sha=`cut -d ' ' -f1 sha2.checksum` 
-  echo -e "{\n\t\"status\":\"success\",\n\t\"md5sum\":\"$md5\",\n\t\"sha2sum\":\"$sha\"\n}" > $JSON_OUT
+    md5=`cut -d ' ' -f1 md5.checksum`
+    sha=`cut -d ' ' -f1 sha2.checksum`
+    echo -e "{\n\t\"status\":\"success\",\n\t\"md5sum\":\"$md5\",\n\t\"sha2sum\":\"$sha\"\n}" > $JSON_OUT
 else
-  echo -e "{\n\t\"status\":\"failed\"\n}" > $JSON_OUT
+    echo -e "{\n\t\"status\":\"failed\"\n}" > $JSON_OUT
 fi
 
 rm -f md5.checksum sha2.checksum
@@ -28,6 +28,6 @@ POST_ADDRESS=$3
 
 if [ ! -z "$POST_ADDRESS" ]
 then
-      curl -X POST -d @"$JSON_OUT" "$POST_ADDRESS"
+    # max connection time is 2mins, 3 mins in total to complete the request
+    curl -X POST -d @"$JSON_OUT" --connect-timeout 120 --max-time 180 "$POST_ADDRESS"
 fi
-
