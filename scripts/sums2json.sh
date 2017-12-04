884649693e95c88907d4ebe71d872fd348886cb9
Yaobo Xu <yx2@sanger.ac.uk>
2017-12-04 17:18:24 +0000

    prevented silent HTTP fail

--- scripts/sums2json.sh
+++ scripts/sums2json.sh
@@ -29,5 +29,6 @@ POST_ADDRESS=$2
 if [ ! -z "$POST_ADDRESS" ]
 then
     # max connection time is 2mins, 3 mins in total to complete the request
-    curl -X POST -d @"$JSON_OUT" --connect-timeout 120 --max-time 180 "$POST_ADDRESS" > post_server_response.txt
+    # add -f to prevent silent HTTP fail on server errors.
+    curl -X POST -d @"$JSON_OUT" --connect-timeout 120 --max-time 180 -f "$POST_ADDRESS" > post_server_response.txt
 fi
